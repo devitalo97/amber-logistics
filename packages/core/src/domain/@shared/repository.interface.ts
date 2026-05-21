708331434508684758081d1f03595330dc28ae09
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-20 21:18:43 -0300

    refactor(package/core): replace partial queries with simple strings

--- packages/core/src/domain/@shared/repository.interface.ts
+++ packages/core/src/domain/@shared/repository.interface.ts
@@ -1,8 +1,8 @@
 interface IRepository<T> {
 	create(data: T): Promise<void>;
-	update(query: Partial<T>, data: T): Promise<void>;
-	delete(query: Partial<T>): Promise<void>;
-	findOne(query: Partial<T>): Promise<T | null>;
+	update(id: string, data: T): Promise<void>;
+	delete(id: string): Promise<void>;
+	findOne(id: string): Promise<T | null>;
 }
 
 export type { IRepository };
