32ff02ca03e3fcf41935dd78140e05e35d9faca6
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-21 10:32:58 -0300

    refactor(package/core): adjust the brand site use case to use an unit of work

--- packages/core/src/application/use-case/brand-site.create.use-case.ts
+++ packages/core/src/application/use-case/brand-site.create.use-case.ts
@@ -1,31 +1,41 @@
 import type { IUseCase } from "@repo/use-case";
+import type { IUnitOfWork } from "@/domain/@shared/unit-of-work.repository.interface";
 import {
 	BrandSite,
 	type BrandSiteData,
+	type BrandSiteEntityInput,
 } from "@/domain/brand-site/brand-site.entity";
 import type { IBrandSiteRepository } from "@/domain/brand-site/brand-site.repository.interface";
-import { Waypoint, type WaypointData } from "@/domain/waypoint/waypoint.entity";
+import {
+	Waypoint,
+	type WaypointEntityInput,
+} from "@/domain/waypoint/waypoint.entity";
 import type { IWaypointRepository } from "@/domain/waypoint/waypoint.repository.interface";
 import type { ITimestampProvider } from "../provider/timestamp-provider.interface";
 import type { IIdProvider } from "../provider/uuid-provider.interface";
 
-interface BrandSiteCreateInput {
-	brandSite: BrandSiteData;
-	waypoint: WaypointData;
+interface UseCaseInput {
+	brandSite: BrandSiteEntityInput;
+	waypoint: WaypointEntityInput;
 }
 
 interface IBrandSiteCreateUseCase
-	extends IUseCase<BrandSiteCreateInput, BrandSiteData> {}
+	extends IUseCase<UseCaseInput, BrandSiteData> {}
+
+interface IBrandSiteAndWaypointUow
+	extends IUnitOfWork<{
+		brandSiteRepository: IBrandSiteRepository;
+		waypointRepository: IWaypointRepository;
+	}> {}
 
 class BrandSiteCreateUseCase implements IBrandSiteCreateUseCase {
 	constructor(
-		private readonly brandSiteRepository: IBrandSiteRepository,
-		private readonly waypointRepository: IWaypointRepository,
+		private readonly uow: IBrandSiteAndWaypointUow,
 		private readonly dateProvider: ITimestampProvider,
 		private readonly idProvider: IIdProvider,
 	) {}
 
-	async execute(input: BrandSiteCreateInput): Promise<BrandSiteData> {
+	async execute(input: UseCaseInput): Promise<BrandSiteData> {
 		const { brandSite, waypoint } = input;
 		const brandSiteEntity = BrandSite.create(
 			brandSite,
@@ -37,6 +47,10 @@ class BrandSiteCreateUseCase implements IBrandSiteCreateUseCase {
 			this.idProvider,
 			this.dateProvider,
 		);
+		await this.uow.transaction(async (context) => {
+			await context.brandSiteRepository.create(brandSiteEntity.toObject());
+			await context.waypointRepository.create(waypointEntity.toObject());
+		});
 		return brandSiteEntity.toObject();
 	}
 }
