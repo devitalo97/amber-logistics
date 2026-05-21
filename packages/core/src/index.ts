5de1bc642bc1745d73b0d21600fa72e0c316a29f
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-21 13:11:20 -0300

    feat(package/core): export artifacts from an index file

---
+++ packages/core/src/index.ts
@@ -0,0 +1,9 @@
+export type {
+	BrandSiteData,
+	BrandSiteEntityInput,
+} from "./domain/brand-site/brand-site.entity";
+export type {
+	WaypointData,
+	WaypointEntityInput,
+} from "./domain/waypoint/waypoint.entity";
+export { CompositionRoot } from "./infrastructure/composition-root";
