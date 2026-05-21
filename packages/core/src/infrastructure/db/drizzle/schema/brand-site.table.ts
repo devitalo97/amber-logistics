3a8d79eb6c96da72f6829b3ae46b7ec927bad4b7
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-20 21:20:46 -0300

    refactor(package/core): change casing style from camelCase to snake_case

--- packages/core/src/infrastructure/db/drizzle/schema/brand-site.table.ts
+++ packages/core/src/infrastructure/db/drizzle/schema/brand-site.table.ts
@@ -9,29 +9,20 @@ import { brandSiteTypeEnum, operationalStatusEnum } from "./enums";
 import { waypointTable } from "./waypoint.table";
 
 export const brandSiteTable = pgTable("brand_site", {
-	id: uuid("id").primaryKey().defaultRandom(),
-	pointId: uuid("point_id")
+	id: uuid().primaryKey().defaultRandom(),
+	point_id: uuid()
 		.notNull()
 		.references(() => waypointTable.id, {
 			onDelete: "cascade",
 		}),
-	type: brandSiteTypeEnum("type").notNull(),
+	type: brandSiteTypeEnum().notNull(),
 
-	storageCapacityCbm: decimal("storage_capacity_cbm", {
-		precision: 10,
-		scale: 2,
-	}).$type<number>(),
+	storage_capacity_cbm: decimal({ precision: 10, scale: 2 }).$type<number>(),
 
-	operationalStatus: operationalStatusEnum("operational_status")
-		.notNull()
-		.default("active"),
+	operational_status: operationalStatusEnum().notNull().default("active"),
 
-	description: varchar("description", { length: 255 }).notNull(),
+	description: varchar({ length: 255 }).notNull(),
 
-	createdAt: timestamp("created_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
-	updatedAt: timestamp("updated_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
+	created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
+	updated_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
 });
--- packages/core/src/infrastructure/db/drizzle/schema/waypoint.table.ts
+++ packages/core/src/infrastructure/db/drizzle/schema/waypoint.table.ts
@@ -9,29 +9,21 @@ import {
 import { waypointTypeEnum } from "./enums";
 
 export const waypointTable = pgTable("waypoint", {
-	id: uuid("id").primaryKey().defaultRandom(),
-	description: varchar("description", { length: 255 }).notNull(),
-	type: waypointTypeEnum("type").notNull(),
+	id: uuid().primaryKey().defaultRandom(),
+	description: varchar({ length: 255 }).notNull(),
+	type: waypointTypeEnum().notNull(),
 
-	latitude: decimal("latitude", { precision: 10, scale: 8 })
-		.$type<number>()
-		.notNull(),
-	longitude: decimal("longitude", { precision: 11, scale: 8 })
-		.$type<number>()
-		.notNull(),
+	latitude: decimal({ precision: 10, scale: 8 }).$type<number>().notNull(),
+	longitude: decimal({ precision: 11, scale: 8 }).$type<number>().notNull(),
 
-	countryCode: char("country_code", { length: 2 }).notNull(),
-	city: varchar("city", { length: 150 }).notNull(),
-	addressLine1: varchar("address_line_1", { length: 200 }).notNull(),
-	addressLine2: varchar("address_line_2", { length: 200 }),
-	postalCode: varchar("postal_code", { length: 20 }).notNull(),
-	state: varchar("state", { length: 50 }).notNull(),
-	timeZone: varchar("time_zone", { length: 50 }),
+	country_code: char({ length: 2 }).notNull(),
+	city: varchar({ length: 150 }).notNull(),
+	address_line_1: varchar({ length: 200 }).notNull(),
+	address_line_2: varchar({ length: 200 }),
+	postal_code: varchar({ length: 20 }).notNull(),
+	state: varchar({ length: 50 }).notNull(),
+	time_zone: varchar({ length: 50 }),
 
-	createdAt: timestamp("created_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
-	updatedAt: timestamp("updated_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
+	created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
+	updated_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
 });
