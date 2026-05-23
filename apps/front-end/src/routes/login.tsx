6d0614ad64b09a16d9fc993c803b52390819472b
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-23 20:39:05 -0300

    fix(front-end): use getSession from 'auth.functions' instead of authClient.getSession

--- apps/front-end/src/routes/login.tsx
+++ apps/front-end/src/routes/login.tsx
@@ -1,10 +1,10 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { LoginForm } from "#/components/forms/auth/login.form";
-import { authClient } from "#/lib/auth-client";
+import { getSession } from "#/lib/auth.functions";
 
 export const Route = createFileRoute("/login")({
 	beforeLoad: async () => {
-		const session = await authClient.getSession();
+		const session = await getSession();
 
 		if (session) {
 			throw redirect({
