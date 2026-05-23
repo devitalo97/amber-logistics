431b53cd0dc51701b79b2ff95a120e44f036ac64
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-23 16:50:00 -0300

    feat(front-end): check if user is authenticated in login page

--- apps/front-end/src/routes/login.tsx
+++ apps/front-end/src/routes/login.tsx
@@ -1,6 +1,16 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { LoginForm } from "#/components/forms/auth/login.form";
+import { authClient } from "#/lib/auth-client";
 
 export const Route = createFileRoute("/login")({
+	beforeLoad: async () => {
+		const session = await authClient.getSession();
+
+		if (session) {
+			throw redirect({
+				to: "/dashboard",
+			});
+		}
+	},
 	component: LoginForm,
 });
