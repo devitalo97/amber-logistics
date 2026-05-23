dcc34118cc8b3c0b33429dadae92db7f96936691
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-23 14:40:13 -0300

    feat(front-end): configure the database on better auth

--- apps/front-end/src/lib/auth.ts
+++ apps/front-end/src/lib/auth.ts
@@ -1,9 +1,21 @@
+import { schema } from "@repo/core";
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { db } from "./db";
 
 export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true,
 	},
+	database: drizzleAdapter(db, {
+		provider: "pg",
+		schema: {
+			user: schema.userTable,
+			session: schema.sessionTable,
+			account: schema.accountTable,
+			verification: schema.verificationTable,
+		},
+	}),
 	plugins: [tanstackStartCookies()],
 });
