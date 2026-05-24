b8da09e27f6cdcfa971cebe6c4c59ca6193b70e1
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-23 21:02:43 -0300

    feat(front-end): remove breadcrumb component

--- apps/front-end/src/components/layout/protected.layout.tsx
+++ apps/front-end/src/components/layout/protected.layout.tsx
@@ -1,13 +1,4 @@
 import { AppSidebar } from "../sidebar/app-sidebar";
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator,
-} from "../ui/breadcrumb";
-import { Separator } from "../ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 
 export function ProtectedLayout({ children }: { children: React.ReactNode }) {
@@ -18,23 +9,6 @@ export function ProtectedLayout({ children }: { children: React.ReactNode }) {
 				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
 					<div className="flex items-center gap-2 px-4">
 						<SidebarTrigger className="-ml-1" />
-						<Separator
-							orientation="vertical"
-							className="mr-2 data-[orientation=vertical]:h-4"
-						/>
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="#">
-										Build Your Application
-									</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator className="hidden md:block" />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
 					</div>
 				</header>
 				{children}
