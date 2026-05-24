8d9c7f6ab2c8cce28ff7c251c9944e2fd43d3b57
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-24 12:24:38 -0300

    feat(front-end): request data to filled up the page on route

--- apps/front-end/src/routes/_protected/logistics/simulator.tsx
+++ apps/front-end/src/routes/_protected/logistics/simulator.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { LogisticsSimulatorPage } from "#/pages/logistics-simulator.page";
 
+import { getProductsFn, getWaypointsFn } from "@/lib/logistics.functions";
+
 export const Route = createFileRoute("/_protected/logistics/simulator")({
+	loader: async () => {
+		const [products, waypoints] = await Promise.all([
+			getProductsFn(),
+			getWaypointsFn(),
+		]);
+		return { products, waypoints };
+	},
 	component: LogisticsSimulatorPage,
 });
