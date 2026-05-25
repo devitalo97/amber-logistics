ade7fa4660200fe5711b773a049af37fe9b0ff6d
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-25 16:37:27 -0300

    feat(front-end): use correct icons

--- apps/front-end/src/components/tables/brand-site/data.ts
+++ apps/front-end/src/components/tables/brand-site/data.ts
@@ -1,21 +1,15 @@
-import {
-	ArrowDown,
-	ArrowRight,
-	ArrowUp,
-	Circle,
-	HelpCircle,
-} from "lucide-react";
+import { Boxes, CheckCircle2, Store, Warehouse, XCircle } from "lucide-react";
 
 export const operationalStatuses = [
 	{
 		value: "active",
 		label: "Active",
-		icon: HelpCircle,
+		icon: CheckCircle2,
 	},
 	{
 		value: "inactive",
 		label: "Inactive",
-		icon: Circle,
+		icon: XCircle,
 	},
 ];
 
@@ -23,16 +17,16 @@ export const types = [
 	{
 		label: "Distribution Center",
 		value: "distribution_center",
-		icon: ArrowDown,
+		icon: Boxes,
 	},
 	{
 		label: "Warehouse",
 		value: "warehouse",
-		icon: ArrowRight,
+		icon: Warehouse,
 	},
 	{
 		label: "Retail Store",
 		value: "retail_store",
-		icon: ArrowUp,
+		icon: Store,
 	},
 ];
