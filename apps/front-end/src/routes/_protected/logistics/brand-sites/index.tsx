f89cd37ebcf3aee0a823ad9187ad6183c836699c
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-25 12:05:50 -0300

    feat(front-end): request data on brand site route

--- apps/front-end/src/routes/_protected/logistics/brand-sites/index.tsx
+++ apps/front-end/src/routes/_protected/logistics/brand-sites/index.tsx
@@ -1,5 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { getBrandSitesFn } from "#/lib/logistics.functions";
+import { BrandSiteListPage } from "#/pages/brand-site.list.page";
 
 export const Route = createFileRoute("/_protected/logistics/brand-sites/")({
-	component: () => <div>a</div>,
+	loader: async () => {
+		const brandSites = await getBrandSitesFn();
+		return { brandSites };
+	},
+	component: BrandSiteListPage,
 });
