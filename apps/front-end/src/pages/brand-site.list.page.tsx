57b20019d0a91f98d36ffac96f7a4dc1fa61c149
Italo Colombi de Souza <dev.italo.souza@gmail.com>
2026-05-26 10:50:54 -0300

    feat(front-end): add title on brand site list page

--- apps/front-end/src/pages/brand-site.list.page.tsx
+++ apps/front-end/src/pages/brand-site.list.page.tsx
@@ -45,7 +45,15 @@ export function BrandSiteListPage() {
 	const { data, totalCount } = response;
 
 	return (
-		<div>
+		<div className="hidden h-full flex-1 flex-col gap-8 p-8 md:flex">
+			<div className="flex items-center justify-between gap-2">
+				<div className="flex flex-col gap-1">
+					<h2 className="text-2xl font-semibold tracking-tight">Brand Sites</h2>
+					<p className="text-muted-foreground">
+						Here&apos;s a list of your brand sites.
+					</p>
+				</div>
+			</div>
 			<DataTable
 				columns={brandSiteColumns}
 				data={data}
